fe333c88416c1e5dd0b2e0a4198a1fafe632c353
user <glvlglvl01@naver.com>
2025-04-09 11:08:33 +0900

    add prime print

--- mynumber.c
+++ mynumber.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+bool istrue(int num){
+ if(num==1) return false;
+ else if(num==2) return true;
+ else{
+  for(int i=2;i<=num-1;i++){
+   if(num%i==0)return false;
+   }
+  return true;
+ }
+}
+
 
 int main(){
  for(int i=1;i<=100;i++){
@@ -6,5 +17,8 @@ int main(){
  }
  for(int i=1;i<=100;i++){
   if(i%2==0) printf("%d\n",i);
+ }
+ for(int i=1;i<=100;i++){
+  if(istrue(i)) printf("%d\n",i);
  }
 }
